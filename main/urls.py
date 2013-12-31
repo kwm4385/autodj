1a85d09706fe8568b3124f45014750a90eed91e9
Kevin Moses <kwm4385@rit.edu>
2013-12-31 17:42:13 -0500

    Removed main app urls.py

--- main/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, include, url
-
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'autodj.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'', 'main.views.RegisterView'),
-)
